1643fb507f913666ea2eb94e4401b2918ce9f18c
Sushant Dahal <dahal.sushant.007@gmail.com>
2024-12-19 10:45:07 +0545

    initial empty MAUI project

---
+++ PayTrack/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PayTrack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ PayTrack/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PayTrack
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
